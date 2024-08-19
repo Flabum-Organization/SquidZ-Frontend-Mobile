ad84f02ec564b89e3a762011138a2e0968678e49
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-08-19 00:24:27 -0500

    feat: added operations to data update service.

--- src/dataUpdate/services/DataUpdate.service.js
+++ src/dataUpdate/services/DataUpdate.service.js
@@ -33,4 +33,35 @@ export class DataUpdateService {
             throw new Error('New password should not be the same as the current password.');
         }
     }
+
+    async getUser() {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/users/get-user`, {
+                withCredentials: true,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                }
+            });
+            return response.data;
+        } catch (error) {
+            throw new Error('Error al obtener los datos del usuario.');
+        }
+    }
+
+    async updateUser(userData) {
+        try {
+            const response = await axios.put(`${API_BASE_URL}/users/update-user-data`, userData, {
+                withCredentials: true,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                }
+            });
+            return response;
+        } catch (error) {
+            throw new Error('Error al actualizar los datos del usuario.');
+        }
+    }
+
 }
